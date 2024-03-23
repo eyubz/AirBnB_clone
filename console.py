065d978e46b4c028c3ce4fac3c7c7fe485730515
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-03-23 13:01:04 -0700

    Create command implemented.

--- console.py
+++ console.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ A module contain the command interpretor for BaseModel class """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ Command processor for Base Model class """
 
     prompt = "(hbnb) "
+    __classes = {"BaseModel"}
 
     """ Handle end of file """
     def do_EOF(self, arg):
@@ -23,6 +25,21 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ This does not execute anything """
         pass
+
+    """ Create a new instance """
+    def do_create(self, arg):
+        """ Create an instance of a given object
+        and saves it to json file """
+        args = arg.split()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            class_name = args[0].strip()
+            obj = eval(class_name)()
+            obj.save()
+            print(obj.id)
 
 
 if __name__ == '__main__':
