a4df74f4652a62bc48a91b05faf12cf664cad68b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-03-23 13:58:19 -0700

    Show command implemented.

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ A module contain the command interpretor for BaseModel class """
 import cmd
 from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -31,7 +32,7 @@ class HBNBCommand(cmd.Cmd):
         """ Create an instance of a given object
         and saves it to json file """
         args = arg.split()
-        if len(arg) == 0:
+        if len(args) == 0:
             print("** class name missing **")
         elif args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
@@ -40,6 +41,28 @@ class HBNBCommand(cmd.Cmd):
             obj = eval(class_name)()
             obj.save()
             print(obj.id)
+
+    """ Show string representation """
+    def do_show(self, arg):
+        """ Print string representation of a class
+        based on the  class name """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            objects = models.storage.all()
+            for obj in objects.values():
+                o = obj.to_dict()
+                if (o["__class__"] == args[0].strip()
+                        and o["id"] == args[1].strip()):
+                    print(eval(args[0].strip())())
+                    break
+            else:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
