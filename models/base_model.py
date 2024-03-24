91c3b4a9d0efc71b742307289f056a31be8b52a5
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-03-24 07:34:43 -0700

    File updated.

--- models/base_model.py
+++ models/base_model.py
@@ -51,8 +51,7 @@ class BaseModel:
         And add __class__ key with value class name to the
         dictionary representation """
         new_dict = self.__dict__.copy()
-        for key in new_dict:
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = new_dict[key].isoformat()
+        new_dict["created_at"] = new_dict[key].isoformat()
+        new_dict["updated_at"] = new_dict[key].isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
