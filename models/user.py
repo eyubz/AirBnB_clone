f05ed246d69a2ebb6aa31fd8e1ee30caf7417cc6
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-03-24 01:06:50 -0700

    File updated.

--- models/user.py
+++ models/user.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-""" This modeule contains a User class """
+""" This module contains a User class """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ A class defining a user """
+    """ A class defining a user
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
     email = ""
     password = ""
     first_name = ""
